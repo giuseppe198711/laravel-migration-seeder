80af1000d83762f246105744909e1764a8bfb8d0
Giuseppe <giuseppedf8711@gmail.com>
2020-11-02 16:56:05 +0100

    migration

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
